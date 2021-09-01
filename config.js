dbd00107c919e73d2f36380faac85a9dee45e6db
Markus Hunsalz <dev@hunsalz.de>
2021-09-01 12:10:10 +0200

    added PATH_PREFIX

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/images/website-icon.png',
-  pathPrefix: `/gatsby-starter-aerial/`, // This path is subpath of your hosting https://domain/portfolio
-  // social
+  pathPrefix: process.env.PATH_PREFIX,
 
   authorName: 'Markus Hunsalz',
   authorHeading: 'Re-think common design patterns',
