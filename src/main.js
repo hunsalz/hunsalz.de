c25a74b678458650a0a2a81e21e889a87c8cd59b
Markus Hunsalz <dev@hunsalz.de>
2021-09-03 09:28:52 +0200

    format change - skip ci

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 // Import Tailwind CSS
-require("~/main.css");
+require("~/main.css")
 
 //Remark Syntax Highlighting
-require("gridsome-plugin-remark-prismjs-all/themes/night-owl.css");
-require("prismjs/plugins/line-numbers/prism-line-numbers.css");
-require("prismjs/plugins/command-line/prism-command-line.css");
+require("gridsome-plugin-remark-prismjs-all/themes/night-owl.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
 
-import DefaultLayout from "~/layouts/Default.vue";
+import DefaultLayout from "~/layouts/Default.vue"
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-  Vue.component("Layout", DefaultLayout);
+  Vue.component("Layout", DefaultLayout)
 }
