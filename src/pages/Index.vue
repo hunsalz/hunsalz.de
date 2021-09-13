a16e1fc82d95ff31a334c4d55e8c1845123786c0
Markus Hunsalz <dev@hunsalz.de>
2021-09-13 14:51:27 +0200

    changed layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <Layout>
-    <div class="flex h-screen justify-center items-center bg-green-50">
+    <div class="flex h-screen justify-center items-center bg-gray-500">
       <div class="m-auto">
-        <h1 class="text-4xl text-center pb-4">Markus Hunsalz</h1>
-        <p class="text-2xl text-center pb-4">Re-think common design patterns</p>
+        <h1 class="text-4xl text-center pb-4 text-white">Markus Hunsalz</h1>
         <Social />
+        <p class="text-2xl text-center pb-4 text-white">Re-think common design patterns</p>
       </div>
     </div>
   </Layout>
