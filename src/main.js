1428278455b0c4b3bef1cb544b113545332b5a0b
ionos-deploy-now-support <markus.hunsalz@ionos.com>
2022-06-17 15:47:28 +0200

    removed external fonts

--- src/main.js
+++ src/main.js
@@ -2,21 +2,10 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from "~/layouts/Default.vue"
-import '@/assets/styles/app.scss';
-import '@fontsource/overpass/700.css';
+import "@/assets/styles/app.scss"
+import "@fontsource/overpass/700.css"
 
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout)
-
-  // Add meta data to global head
-  head.link.push({
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-  })
-  head.link.push({
-    rel: "stylesheet",
-    href:
-      "https://fonts.googleapis.com/css2?family=Overpass:wght@100;300&display=swap",
-  })
 }
